51624fb92c95e069766dd605615ea539934fa330
andy <andrejbalaz001@gmail.com>
2021-06-11 16:53:21 +0200

    add: more tests

--- test/test_hashing.c
+++ test/test_hashing.c
@@ -1,16 +1,72 @@
 #include <check.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include "../src/hashing.h"
+#include "../src/hashing.c"
 
 uint hash_fn(const void *);
 int   cmp_fn(const void *, const void *);
 
-START_TEST(empty_test){} END_TEST
+START_TEST(empty_test) {} END_TEST
 
-START_TEST(creates_empty_map){
+START_TEST(creates_empty_map) {
     uint size = 5;
     map_t *map = map_create(size, &hash_fn, &cmp_fn);    // https://stackoverflow.com/a/16917159
     ck_assert_int_eq(map_get_size(map), 0);
+    map_destroy(map);
+    map = NULL;
+} END_TEST
+
+START_TEST(creates_map_of_the_correct_size) {
+    uint sizes[] = {5, 8, 11};
+    for (uint i=0; i<3; i++) {
+        map_t *map = map_create(sizes[i], &hash_fn, &cmp_fn);
+        ck_assert_int_eq(map_get_max_size(map), sizes[i]);
+        map_destroy(map);
+        map = NULL;
+    }
+} END_TEST
+
+//START_TEST(destroyed_map_is_deallocated) {
+//    // allocate 32GB in chunks of 1GB
+//    // if you run out of memory, the deallocation does not work
+//    uint size = 1024 * 1024 * 1024 / sizeof (void *);
+//    for (uint i=0; i<32; i++) {
+//        map_t *map = map_create(size, &hash_fn, &cmp_fn);
+//        map_destroy(map);
+//    }
+//    // find some other way of testing?
+//} END_TEST
+
+START_TEST(can_insert_one_item) {
+    map_t *map = map_create(5, &hash_fn, &cmp_fn);
+    uint *item = malloc(sizeof *item);
+    (*item) = 3;
+    map_insert(map, item);
+    ck_assert_int_eq(map_get_size(map), 1);
+} END_TEST
+
+START_TEST(can_insert_more_items) {
+    uint items[] = {3, 5, 7, 11};
+    map_t *map = map_create(5, &hash_fn, &cmp_fn);
+    for (uint i=0; i<4; i++) { map_insert(map, &items[i]); }
+    ck_assert_int_eq(map_get_size(map), 4);
+} END_TEST
+
+START_TEST(resizes_map) {
+    map_t *map = map_create(5, &hash_fn, &cmp_fn);
+    map_resize(map, 23);
+    ck_assert_int_eq(map_get_max_size(map), 23);
+} END_TEST
+
+START_TEST(inserted_items_can_be_found) {
+    uint items[] = {7, 11, 18, 57, 68};
+    map_t *map = map_create(5, &hash_fn, &cmp_fn);
+    for (uint i=0; i<5; i++) { map_insert(map, &items[i]); }
+    for (uint i=0; i<5; i++) {
+        uint *item = map_search(map, &items[i]);
+        ck_assert_int_eq(*item, items[i]);
+    }
 } END_TEST
 
 int main(void) {
@@ -21,6 +77,12 @@ int main(void) {
 
     tcase_add_test(tcase, empty_test);
     tcase_add_test(tcase, creates_empty_map);
+    tcase_add_test(tcase, creates_map_of_the_correct_size);
+    // tcase_add_test(tcase, destroyed_map_is_deallocated);
+    tcase_add_test(tcase, can_insert_one_item);
+    tcase_add_test(tcase, can_insert_more_items);
+    tcase_add_test(tcase, resizes_map);
+    tcase_add_test(tcase, inserted_items_can_be_found);
 
     SRunner *runner = srunner_create(suite);
     srunner_run_all(runner, CK_VERBOSE);
